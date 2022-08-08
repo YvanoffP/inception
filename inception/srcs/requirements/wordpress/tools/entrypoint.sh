c522117a31c3140c23911f357e95bdd0c7290c66
Yvan-Debian <petruzzi.yvan@gmai.com>
2022-08-08 20:06:33 +0200

    mariadb is sometimes quitting. Still have a 403 page.. sned rlep

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # WP="./wp-cli.phar --path=/wordpress"
 cd /var/www/wordpress
+wp core download --allow-root
 
 # $WP core config \
 #    --dbname=$DB_NAME \
@@ -10,14 +11,14 @@ cd /var/www/wordpress
 #    --dbhost=mariadb \
 #    --locale=en_EN
 
-wp core install \
+wp core install --allow-root \
     --url=$WP_URL \
     --title="$WP_TITLE" \
     --admin_user="$WP_ADMIN_USER" \
     --admin_email="$WP_ADMIN_MAIL" \
     --admin_password="$WP_ADMIN_PASSWORD"
 
-wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"
+wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"
 
 cd -
 
