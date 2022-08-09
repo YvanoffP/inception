7f84b1e0530cfbba4cd7d583d4de64a2ebad52f1
Yvan-Debian <petruzzi.yvan@gmai.com>
2022-08-09 19:01:03 +0200

    so close

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# WP="./wp-cli.phar --path=/wordpress"
 cd /var/www/wordpress
+
 wp core download --allow-root
 
-# $WP core config \
-#    --dbname=$DB_NAME \
-#    --dbuser=$DB_USER \
-#    --dbpass=$DB_PASSWORD \
-#    --dbhost=mariadb \
-#    --locale=en_EN
+wp core install \
+    --allow-root \
+    --url=${WP_URL} \
+    --title=${WP_TITLE} \
+    --admin_user=${WP_ADMIN_USER} \
+    --admin_email=${WP_ADMIN_MAIL} \
+    --admin_password=${WP_ADMIN_PASSWORD}
 
-wp core install --allow-root \
-    --url=$WP_URL \
-    --title="$WP_TITLE" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_email="$WP_ADMIN_MAIL" \
-    --admin_password="$WP_ADMIN_PASSWORD"
+wp user create ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
 
-wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"
+mkdir -p /var/www/wordpress/mysite
+mv /var/www/index.html /var/www/wordpress/index.html
 
 cd -
 
-php-fpm7.3 -F
+/usr/sbin/php-fpm7.3 -F
